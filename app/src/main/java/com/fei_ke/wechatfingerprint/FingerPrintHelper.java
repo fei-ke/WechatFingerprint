2309afb529517c62c03f01ff3770cd9a52afebcf
fei-ke <fei@MAC.local>
2017-02-24 03:42:08 +0800

    xposed

---
+++ app/src/main/java/com/fei_ke/wechatfingerprint/FingerPrintHelper.java
@@ -0,0 +1,161 @@
+package com.fei_ke.wechatfingerprint;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+
+/**
+ * Created by fei on 17/2/24.
+ */
+
+public class FingerPrintHelper extends FingerprintManager.AuthenticationCallback {
+    private static final String TAG = "FingerPrintHelper";
+
+    public static final int ENCRYPT_MODE = Cipher.ENCRYPT_MODE;
+    public static final int DECRYPT_MODE = Cipher.DECRYPT_MODE;
+
+    private FingerprintManager    mFingerprintManager;
+    private CancellationSignal    mCancellationSignal;
+    private LocalAndroidKeyStore  mLocalAndroidKeyStore;
+    private LocalSharedPreference mLocalSharedPreference;
+
+    private int mPurpose = ENCRYPT_MODE;
+
+    private Callback mCallback;
+
+    private Context mContext;
+
+    public FingerPrintHelper(Context context) {
+        super();
+
+        mContext = context;
+
+        mLocalAndroidKeyStore = new LocalAndroidKeyStore();
+        mLocalSharedPreference = new LocalSharedPreference(context);
+
+        if (!mLocalSharedPreference.containsKey(LocalSharedPreference.KEY_PASSWORD)) {
+            mLocalAndroidKeyStore.generateKey(LocalAndroidKeyStore.KEY_NAME);
+        }
+
+        mFingerprintManager = context.getSystemService(FingerprintManager.class);
+    }
+
+    public void setPurpose(int purpose) {
+        mPurpose = purpose;
+    }
+
+    public void setCallback(Callback callback) {
+        mCallback = callback;
+    }
+
+    public void startAuthenticate() {
+        String IV = mLocalSharedPreference.getData(LocalSharedPreference.KEY_IV);
+
+        if (TextUtils.isEmpty(IV)) {
+            mPurpose = ENCRYPT_MODE;
+        }
+
+        mCancellationSignal = new CancellationSignal();
+        if (mContext.checkSelfPermission(Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(mContext, "need permission", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        mFingerprintManager.authenticate(
+                mLocalAndroidKeyStore.getCryptoObject(mPurpose, Base64.decode(IV, Base64.URL_SAFE)),
+                mCancellationSignal,
+                0,
+                this,
+                null);
+    }
+
+
+    public void stopAuthenticate() {
+        if (mCancellationSignal != null) {
+            mCancellationSignal.cancel();
+            mCancellationSignal = null;
+        }
+    }
+
+    @Override
+    public void onAuthenticationError(int errorCode, CharSequence errString) {
+        super.onAuthenticationError(errorCode, errString);
+
+        if (mCallback != null) mCallback.onFailure(errString);
+
+        Log.d(TAG, "onAuthenticationError() called with: errorCode = [" + errorCode + "], errString = [" + errString + "]");
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+        super.onAuthenticationHelp(helpCode, helpString);
+
+        if (mCallback != null) mCallback.onFailure(helpString);
+
+        Log.d(TAG, "onAuthenticationHelp() called with: helpCode = [" + helpCode + "], helpString = [" + helpString + "]");
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        super.onAuthenticationSucceeded(result);
+        Log.d(TAG, "onAuthenticationSucceeded() called with: result = [" + result + "]");
+
+        final Cipher cipher = result.getCryptoObject().getCipher();
+
+        if (mPurpose == ENCRYPT_MODE) {
+            try {
+                byte[] encrypted = cipher.doFinal("123456".getBytes());
+                byte[] IV = cipher.getIV();
+
+                String encryptedText = Base64.encodeToString(encrypted, Base64.URL_SAFE);
+                String IVText = Base64.encodeToString(IV, Base64.URL_SAFE);
+
+                mLocalSharedPreference.storeData(LocalSharedPreference.KEY_PASSWORD, encryptedText);
+                mLocalSharedPreference.storeData(LocalSharedPreference.KEY_IV, IVText);
+
+                Log.i(TAG, "encryptedText: " + encryptedText);
+
+            } catch (IllegalBlockSizeException e) {
+                e.printStackTrace();
+            } catch (BadPaddingException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                String data = mLocalSharedPreference.getData(LocalSharedPreference.KEY_PASSWORD);
+                byte[] decrypted = cipher.doFinal(Base64.decode(data, Base64.URL_SAFE));
+
+                if (mCallback != null) mCallback.onSuccess(new String(decrypted));
+
+            } catch (BadPaddingException | IllegalBlockSizeException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        super.onAuthenticationFailed();
+
+        if (mCallback != null) mCallback.onFailure("error");
+
+        Log.d(TAG, "onAuthenticationFailed() called");
+    }
+
+    public interface Callback {
+        void onSuccess(String value);
+
+        void onFailure(CharSequence helpString);
+    }
+}
--- app/src/main/java/com/fei_ke/wechatfingerprint/Hook.java
+++ app/src/main/java/com/fei_ke/wechatfingerprint/Hook.java
@@ -1,9 +1,128 @@
 package com.fei_ke.wechatfingerprint;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
 /**
  * Created by fei on 2017/2/23.
  */
 
-public class Hook {
-    //com.tencent.mm.plugin.wallet_core.ui.l
+public class Hook implements IXposedHookLoadPackage {
+    private FingerPrintHelper mFingerPrintHelper;
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.tencent.mm")) {
+            hook(lpparam);
+        }
+    }
+
+    private void hook(XC_LoadPackage.LoadPackageParam lpparam) {
+        Class clazzPswDialog = XposedHelpers.findClass(" com.tencent.mm.plugin.wallet_core.ui.l", lpparam.classLoader);
+
+        XposedHelpers.findAndHookMethod(clazzPswDialog, "onCreate", Bundle.class, new XC_MethodHook() {
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                try {
+                    final Dialog dialog = (Dialog) param.thisObject;
+                    int identifier = dialog.getContext().getResources().getIdentifier("a1b", "id", "com.tencent.mm");
+                    final View layoutKeyboard = dialog.getWindow().findViewById(identifier);
+
+                    ViewGroup viewGroup = (ViewGroup) layoutKeyboard.getParent();
+                    viewGroup.removeView(layoutKeyboard);
+
+                    Context thisContext = dialog.getContext().createPackageContext("com.fei_ke.wechatfingerprint", Context.CONTEXT_IGNORE_SECURITY);
+                    final FrameLayout rootLayout = (FrameLayout) View.inflate(thisContext, R.layout.layout_fingerprint, null);
+                    rootLayout.addView(layoutKeyboard, 0);
+
+                    viewGroup.addView(rootLayout);
+                    final View layoutFingerprint = rootLayout.findViewById(R.id.layout_fingerprint);
+                    final TextView textViewHint = (TextView) rootLayout.findViewById(R.id.tv_hint);
+                    final ImageView imageView = (ImageView) rootLayout.findViewById(R.id.ic_fp);
+
+                    rootLayout.findViewById(R.id.view_header).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            TransitionManager.beginDelayedTransition(rootLayout, new Slide());
+                            if (layoutFingerprint.getVisibility() != View.VISIBLE) {
+                                layoutFingerprint.setVisibility(View.VISIBLE);
+                            } else {
+                                layoutFingerprint.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+
+                    int idPwdEditText = dialog.getContext().getResources().getIdentifier("b6", "id", "com.tencent.mm");
+                    final EditText editText = (EditText) dialog.getWindow().findViewById(idPwdEditText);
+
+                    mFingerPrintHelper = new FingerPrintHelper(dialog.getContext());
+                    mFingerPrintHelper.setPurpose(FingerPrintHelper.DECRYPT_MODE);
+                    mFingerPrintHelper.setCallback(new FingerPrintHelper.Callback() {
+                        @Override
+                        public void onSuccess(String value) {
+                            editText.setText(value);
+                            imageView.setImageResource(R.drawable.ic_fingerprint_success);
+                            textViewHint.setText("success");
+                        }
+
+                        @Override
+                        public void onFailure(CharSequence helpString) {
+                            imageView.setImageResource(R.drawable.ic_fingerprint_error);
+                            textViewHint.setText(helpString);
+
+                            //reset
+                            textViewHint.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    imageView.setImageResource(R.drawable.ic_fp);
+                                    textViewHint.setText(null);
+                                }
+                            }, 1000);
+                        }
+                    });
+                    mFingerPrintHelper.startAuthenticate();
+
+                } catch (Throwable t) {
+                    log(t);
+                }
+            }
+        });
+
+
+        XposedHelpers.findAndHookMethod(clazzPswDialog, "dismiss", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                if (mFingerPrintHelper != null) {
+                    mFingerPrintHelper.stopAuthenticate();
+                    mFingerPrintHelper = null;
+                }
+            }
+        });
+    }
+
+    private void log(String log) {
+        XposedBridge.log(log);
+    }
+
+    private void log(Throwable t) {
+        XposedBridge.log(t);
+    }
+
 }
