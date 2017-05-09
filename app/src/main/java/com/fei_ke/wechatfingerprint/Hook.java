ae1c769da8f24e817fc46c553ad0132c4629c64d
fei-ke <jinyang656@qq.com>
2017-05-09 18:11:23 +0800

    use another way to find necessary hook info

--- app/src/main/java/com/fei_ke/wechatfingerprint/Hook.java
+++ app/src/main/java/com/fei_ke/wechatfingerprint/Hook.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import javax.crypto.Cipher;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -26,6 +29,12 @@ public class Hook implements IXposedHookLoadPackage {
 
     private FingerPrintHelper mFingerPrintHelper;
 
+    private Class  classAdapter;
+    private Field  fieldAdapterInSettingUI;
+    private String methodNameOnItemClickInSettingUI;
+    private Field  fieldPreferenceKey;
+    private Class  classPswDialog;
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals(WECHAT_PACKAGE_NAME)) {
@@ -34,59 +43,58 @@ public class Hook implements IXposedHookLoadPackage {
     }
 
     private void hook(XC_LoadPackage.LoadPackageParam lpparam) {
-        final Class clazzPswDialog = XposedHelpers.findClass("com.tencent.mm.plugin.wallet_core.ui.l", lpparam.classLoader);
-        //final Class clazzKeyboardWindow = XposedHelpers.findClass("com.tenpay.android.wechat.MyKeyboardWindow", lpparam.classLoader);
-        //final Class clazzEditText = XposedHelpers.findClass("com.tenpay.android.wechat.TenpaySecureEditText", lpparam.classLoader);
+        //find classPswDialog
+        Class<?> classTmp = XposedHelpers.findClass("com.tencent.mm.plugin.wallet.balance.ui.WalletBalanceFetchPwdInputUI", lpparam.classLoader);
+        for (Field field : classTmp.getDeclaredFields()) {
+            if (field.getType() != String.class) {
+                classPswDialog = field.getType();
+            }
+        }
 
-        XposedHelpers.findAndHookMethod(clazzPswDialog, "onCreate", Bundle.class, new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod(classPswDialog, "onCreate", Bundle.class, new XC_MethodHook() {
 
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                try {
-                    final Dialog dialog = (Dialog) param.thisObject;
-                    final View layoutKeyboard = (View) Util.findViewByClass(dialog.getWindow().getDecorView(),
-                            "com.tenpay.android.wechat.MyKeyboardWindow")
-                            .getParent();
+                final Dialog dialog = (Dialog) param.thisObject;
+                final View layoutKeyboard = (View) Util.findViewByClass(dialog.getWindow().getDecorView(), "com.tenpay.android.wechat.MyKeyboardWindow")
+                        .getParent();
 
-                    ViewGroup container = (ViewGroup) layoutKeyboard.getParent();
-                    container.removeView(layoutKeyboard);
+                ViewGroup container = (ViewGroup) layoutKeyboard.getParent();
+                container.removeView(layoutKeyboard);
 
-                    Context remoteContext = dialog.getContext().createPackageContext(BuildConfig.APPLICATION_ID, Context.CONTEXT_IGNORE_SECURITY);
-                    final FingerPrintLayout fingerPrintLayout = new FingerPrintLayout(remoteContext);
-                    fingerPrintLayout.addView(layoutKeyboard, 0);
+                Context remoteContext = dialog.getContext().createPackageContext(BuildConfig.APPLICATION_ID, Context.CONTEXT_IGNORE_SECURITY);
+                final FingerPrintLayout fingerPrintLayout = new FingerPrintLayout(remoteContext);
+                fingerPrintLayout.addView(layoutKeyboard, 0);
 
-                    container.addView(fingerPrintLayout);
+                container.addView(fingerPrintLayout);
 
 
-                    final EditText editText = (EditText) Util.findViewByClass(container, "com.tenpay.android.wechat.TenpaySecureEditText");
+                final EditText editText = (EditText) Util.findViewByClass(container, "com.tenpay.android.wechat.TenpaySecureEditText");
 
-                    mFingerPrintHelper = new FingerPrintHelper(dialog.getContext(), fingerPrintLayout);
-                    mFingerPrintHelper.setPurpose(FingerPrintHelper.DECRYPT_MODE);
-                    mFingerPrintHelper.setCallback(new FingerPrintHelper.Callback() {
-                        @Override
-                        public void onSuccess(int purpose, Cipher cipher) {
-                            String pwd = mFingerPrintHelper.decrypt(cipher);
-                            if (!TextUtils.isEmpty(pwd)) {
-                                editText.setText(pwd);
-                            }
+                mFingerPrintHelper = new FingerPrintHelper(dialog.getContext(), fingerPrintLayout);
+                mFingerPrintHelper.setPurpose(FingerPrintHelper.DECRYPT_MODE);
+                mFingerPrintHelper.setCallback(new FingerPrintHelper.Callback() {
+                    @Override
+                    public void onSuccess(int purpose, Cipher cipher) {
+                        String pwd = mFingerPrintHelper.decrypt(cipher);
+                        if (!TextUtils.isEmpty(pwd)) {
+                            editText.setText(pwd);
                         }
+                    }
 
-                        @Override
-                        public void onFailure(CharSequence helpString) {
+                    @Override
+                    public void onFailure(CharSequence helpString) {
 
-                        }
-                    });
+                    }
+                });
 
-                    mFingerPrintHelper.startAuthenticate();
+                mFingerPrintHelper.startAuthenticate();
 
-                } catch (Throwable t) {
-                    log(t);
-                }
             }
         });
 
 
-        XposedHelpers.findAndHookMethod(clazzPswDialog, "dismiss", new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod(classPswDialog, "dismiss", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 if (mFingerPrintHelper != null) {
@@ -96,30 +104,64 @@ public class Hook implements IXposedHookLoadPackage {
             }
         });
 
-        Class classWalletPasswordSettingUI = XposedHelpers.findClass("com.tencent.mm.plugin.wallet.pwd.ui.WalletPasswordSettingUI", lpparam.classLoader);
+        final Class classWalletPasswordSettingUI = XposedHelpers.findClass("com.tencent.mm.plugin.wallet.pwd.ui.WalletPasswordSettingUI", lpparam.classLoader);
         final Class classPreference = XposedHelpers.findClass("com.tencent.mm.ui.base.preference.Preference", lpparam.classLoader);
-        Class classAdapter = XposedHelpers.findClass("com.tencent.mm.ui.base.preference.f", lpparam.classLoader);
 
+        //find adapter class
+        Method[] methods = classWalletPasswordSettingUI.getMethods();
+        for (Method method : methods) {
+            Class<?> returnType = method.getReturnType();
+            Class<?>[] parameterTypes = method.getParameterTypes();
+
+            if (returnType == boolean.class && parameterTypes.length == 2 && parameterTypes[1] == classPreference) {
+                classAdapter = parameterTypes[0];
+                methodNameOnItemClickInSettingUI = method.getName();
+                break;
+            }
+        }
+
+        //find fieldAdapterInSettingUI
+        Field[] declaredFields = classWalletPasswordSettingUI.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.getType() == classAdapter) {
+                field.setAccessible(true);
+                fieldAdapterInSettingUI = field;
+                break;
+            }
+        }
+
+        //find fieldPreferenceKey
+        Field[] fields = classPreference.getFields();
+        for (Field field : fields) {
+            if (field.getType() == String.class) {
+                fieldPreferenceKey = field;
+                break;
+            }
+        }
         XposedHelpers.findAndHookMethod(classWalletPasswordSettingUI, "onCreate", Bundle.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Object thisObject = param.thisObject;
-                Object adapter = XposedHelpers.getObjectField(thisObject, "dzO");
+                Object adapter = fieldAdapterInSettingUI.get(thisObject);
 
+                //create a clickable item
                 Object preference = XposedHelpers.newInstance(classPreference, (Context) thisObject);
                 XposedHelpers.callMethod(preference, "setKey", "xposed_wechat_fingerprint");
                 XposedHelpers.callMethod(preference, "setTitle", (CharSequence) "Xposed 指纹支付");
 
+                //add to settingUI
                 int index = (int) XposedHelpers.callMethod(adapter, "indexOf", "wallet_open_gesture_password");
-                XposedHelpers.callMethod(adapter, "a", new Class[]{classPreference, int.class}, preference, index);
+                Method methodAddItem = XposedHelpers.findMethodsByExactParameters(classAdapter, void.class, classPreference, int.class)[0];
+                methodAddItem.invoke(adapter, preference, index);
             }
         });
 
-        XposedHelpers.findAndHookMethod(classWalletPasswordSettingUI, "a", classAdapter, classPreference, new XC_MethodHook() {
+
+        XposedHelpers.findAndHookMethod(classWalletPasswordSettingUI, methodNameOnItemClickInSettingUI, classAdapter, classPreference, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Object preference = param.args[1];
-                String key = (String) XposedHelpers.getObjectField(preference, "dqE");
+                String key = (String) fieldPreferenceKey.get(preference);
                 if ("xposed_wechat_fingerprint".equals(key)) {
                     Activity activity = (Activity) param.thisObject;
                     SetPasswordFragment fragment = new SetPasswordFragment();
